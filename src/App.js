7eb4529718176e421ec0f45d5b93c5b9b00c1ea7
Rodney Mupanduki <rodneym@afrosoft.co.zw>
2021-12-11 10:56:36 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import {
+  Routes,
+  Route
+} from "react-router-dom";
 import Navbar from './components/Navbar'
 import About from './pages/About'
 import Contact from './pages/Contact'
@@ -10,15 +13,17 @@ import Services from './pages/Services'
 const App = () => {
   return (
     <>
-    <Router>
       <Navbar />
-      <Switch>
-          <Route to='/' exact component={Home} />
-          <Route to='/about' exact component={About} />
-          <Route to='/contact' exact component={Contact} />
-          <Route to='/services' exact component={Services} />
-      </Switch>
-      </Router>
+      <Routes>
+          <Route path='/'  element={<Home />} />
+          <Route path='/about'  element={<About />} />
+          <Route path='/contact'  element={<Contact />} />
+          <Route path='/services'  element={<Services />} />
+      </Routes>
+
+
+
+    
     </>
   )
 }
